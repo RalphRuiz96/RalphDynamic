309c725fd1b78c9dc48c5a649e59543defc8dd79
unknown <ruizra@kean.edu>
2020-12-29 22:50:18 -0500

    Ralph - more adjustments to the same pages

--- JavaScriptForP1.js
+++ JavaScriptForP1.js
@@ -4,6 +4,11 @@ var UserName = null;
 window.onload = function () {
     do{
         UserName = prompt("Please enter your name");
+        if (UserName == null || UserName == "") {
+            continue;
+        } else {
+            break;
+        }
     } while (UserName == null || UserName == "");
     
     document.getElementById("demo").innerHTML = "Hello " + UserName + ", you are in the first page of the project!";
