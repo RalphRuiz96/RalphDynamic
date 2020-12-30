23972168ac5b6e9cb82f992c8a45a86393acd137
unknown <ruizra@kean.edu>
2020-12-29 22:59:09 -0500

    Ralph - made a new concept in the javascript page

--- JavaScriptForP1.js
+++ JavaScriptForP1.js
@@ -2,14 +2,10 @@
 var UserName = null;
 
 window.onload = function () {
-    do{
-        UserName = prompt("Please enter your name");
-        if (UserName != null || UserName != "") {
-            break;
-        } else {
-            continue;
-        }
-    } while (UserName == null || UserName == "");
-    
+    UserName = prompt("Please enter your name");
+
+    if (UserName == null || UserName == "") { 
+        UserName = "Mr. Nobody";
+    }
     document.getElementById("demo").innerHTML = "Hello " + UserName + ", you are in the first page of the project!";
 }
